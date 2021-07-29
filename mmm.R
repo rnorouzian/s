a65f1bba23f6772868b09f1890510f55206a197d
Reza Norouzian <rnorouzian@gmail.com>
2021-07-28 21:15:12 -0500

    Update mmm.R

--- mmm.R
+++ mmm.R
@@ -50,7 +50,7 @@ data.tree_ <- function(data, toplab = NULL, cex = 1, ...){
 
 #===============================================================================================================================
 
-study_tree <- function(data, study_col, grp2_col, grp3_col, time_col, study_name = NULL, reset = TRUE,
+study_tree_limited <- function(data, study_col, grp2_col, grp3_col, time_col, study_name = NULL, reset = TRUE,
                        structure = c("simple","typical","complex"), output_studies = FALSE,
                        toplab = NULL, cex = 1)
 {
@@ -129,6 +129,86 @@ study_tree <- function(data, study_col, grp2_col, grp3_col, time_col, study_name
 
 #===============================================================================================================================
 
+study_tree <- function(data, highest_level, ..., highest_level_name = NULL, reset = TRUE,
+                 structure = c("simple","typical","complex"), output_highest_level = FALSE,
+                 toplab = NULL, cex = 1) 
+  {
+  
+  data <- rm.colrowNA(trim_(data))
+  
+  dot_cols <- rlang::ensyms(...)
+  str_cols <- purrr::map_chr(dot_cols, rlang::as_string)
+  
+  ss <- substitute(highest_level)
+  sss <- deparse(ss)
+  
+  if(reset){
+    graphics.off()
+    org.par <- par(no.readonly = TRUE)
+    on.exit(par(org.par))
+  }
+  
+  data <- data %>%
+    dplyr::select({{highest_level}}, !!! dot_cols)
+    
+  if(is.null(highest_level_name)){
+    
+    struc <- match.arg(structure) 
+  
+ hlist <- data %>%
+    dplyr::group_by({{highest_level}}) %>%
+    dplyr::mutate(grp = across(all_of(str_cols), ~ n_distinct(.) == 1) %>%
+                    purrr::reduce(stringr::str_c, collapse="")) %>%
+    dplyr::ungroup(.) %>%
+    dplyr::group_split(grp, .keep = FALSE)
+ 
+ res <- Filter(NROW, rev(hlist))
+ 
+ main_title <- sapply(res, function(i) length(unique(i[[sss]])))
+ 
+ typic <- function(vec) vec[ceiling(length(vec)/2)]
+ 
+ nms <- lapply(res, function(i){
+   nr <- sapply(split(i, i[[sss]]), nrow);
+   study_type <- if(struc == "typical") {typic(as.numeric(names(table(nr))))
+   } else if(struc == "simple") {min(as.numeric(names(table(nr))))
+   } else {max(as.numeric(names(table(nr))))};
+   names(nr)[nr == study_type][1]
+ })
+ 
+ list2plot <- lapply(seq_along(res),function(i) subset(res[[i]], eval(ss) == nms[i]))
+ 
+ LL <- length(list2plot)
+ 
+ if(LL > 1L) { par(mfrow = n2mfrow(LL)) }
+ 
+ main <- paste(main_title,"studies")
+ 
+ invisible(lapply(seq_along(list2plot), function(i) data.tree_(list2plot[[i]], main = main[i], toplab, cex)))
+ 
+ if(output_highest_level) res
+ 
+  } else {
+    
+    highest_level_name <- trimws(highest_level_name)
+    highest_level_names <- unique(data[[sss]])
+    
+    idx <- highest_level_name %in% highest_level_names 
+    
+    if(!all(idx)) stop(dQuote(toString(highest_level_name[!idx]))," not found in the ", paste0("'",sss,"'", " column."), call. = FALSE)
+    
+    list2plot <- lapply(highest_level_name, function(i) subset(data, eval(ss) == i))
+    
+    LL <- length(list2plot)
+    
+    if(LL > 1L) { par(mfrow = n2mfrow(LL))}
+    
+    invisible(lapply(list2plot, data.tree_, toplab, cex))
+  }
+}                        
+                        
+#===============================================================================================================================
+                        
 needzzsf <- c("plotrix","tidyverse")    
 
 not.have23 <- needzzsf[!(needzzsf %in% installed.packages()[,"Package"])]
